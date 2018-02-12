9cc89f6c91b9cb204455f31d379ebb54c67f3f85
Gabriel Ruiz <gabe@ggruiz.me>
2018-02-11 23:26:34 -0800

    Initial Commit

---
+++ SwiftEulerProblems/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftEulerProblems
+//
+//  Created by Gabriel Ruiz on 2/11/18.
+//  Copyright © 2018 Gabriel Ruiz. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
